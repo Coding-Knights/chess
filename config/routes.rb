a30eca579bf00571a75ebc202c3add19ab9d6655
Al Trujillo <Trujillo0525@gmail.com>
2020-01-23 03:20:55 +0000

    removed extra lines in routes page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-=======
-end
 
- # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> 02f14aed4ea17083d3ad2ed368bbaf66d8baa030
+end
