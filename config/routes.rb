f813d3360620d9b51f78c7ed574599369e21927b
Al Trujillo <Trujillo0525@gmail.com>
2020-01-23 04:27:20 +0000

    added devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-
+  devise_for :models
+# Needed to get devise installation complete(Al Trujillo)
+#Ensure you have defined root_url to *something* in your config/routes.rb.
+#     For example:
+#       root to: "home#index"
 end
