85a8402c7303656fc22b338881ec57708cb86d5a
Vincent-Job <vinjob30@gmail.com>
2020-01-29 04:11:49 +0000

    Create resources for pieces

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :users
 	  root 'chess#index'
+
+	  resources :pawns
+	  resources :knights
+	  resources :bishops
+	  resources :rooks
+	  resources :queens
+	  resources :kings
+
 end
