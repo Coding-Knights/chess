37589915452aa56f2b995b7f71799b4133df4af2
Joe <joeholley@me.com>
2020-01-30 21:20:34 +0000

    Added changes to list available games on homepage

--- app/controllers/chess_controller.rb
+++ app/controllers/chess_controller.rb
@@ -1,4 +1,8 @@
 class ChessController < ApplicationController
 	def index
+    #Once scope task is complete and game.available can be called then will delete line 6 and uncomment line 5
+    #For now is calling all games but since we have no games yet will not show any games on home page    
+    #@games = Game.available
+     @games = Game.all
 	end
 end
