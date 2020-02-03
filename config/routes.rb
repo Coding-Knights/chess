5a1ad3758605c340aff57c883ce466a7967867db
raquibbi <raquibbi@gmail.com>
2020-02-03 18:49:14 +0000

    added new route in config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 	  root 'chess#index'
-    resources :games, only: [:show] 
+    resources :games, only: [:show, :create, :update] 
 
 	  resources :pawns
 	  resources :knights
