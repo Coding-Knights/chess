7aa6f176f2099c4e4989c6927f1ada18214ec55d
Al Trujillo <Trujillo0525@gmail.com>
2020-02-07 00:24:39 +0000

    added color field to pieces table

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,13 +9,10 @@ class GamesController < ApplicationController
 	end
 
 	def new
-
-		@game = Game.new
-    
+		@game = Game.new  
 	end
 
-	def create
-    
+	def create 
 		enemyID = gameParams[:black_player_id]
 		gameName = gameParams[:name]
 		@game = Game.create(:black_player_id => enemyID, :white_player_id => current_user.id, :name => gameName)
