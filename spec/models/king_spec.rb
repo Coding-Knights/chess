dc0568769b60d8c238d5144c7257d04c49567084
jaykibi <raquibbi@gmail.com>
2020-02-16 02:23:06 +0000

    fixed logic in king_spec.rb and added to factories.rb

--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -8,18 +8,20 @@ RSpec.describe Game, type: :model do
 end
 
 RSpec.describe King, type: :model do
-  it "should check if king at (4,0) exists" do
-    board = FactoryBot.create(:game)
-    king = board.pieces.where(x_position: '4', y_position: '0')
+  # it "should check if king at (4,0) exists" do
+  #   board = FactoryBot.create(:game)
+  #   king = board.pieces.where(x_position: '4', y_position: '0')
 
-    expect(king).not_to be_empty
-  end
+  #   expect(king).not_to be_empty
+  # end
 
-  it 'should test if horizontal moves are valid' do      
-    expect(@king.valid_move?(5, 0)).to eq true
+  it 'should test if diagonal moves are valid' do
+    @king = FactoryBot.create(:king, x_position: '4', y_position: '4')
+    expect(@king.valid_move?(5,5)).to eq true
   end
 
   it 'should not allow horizontal movements greater than 1' do
+    @king = FactoryBot.create(:king, x_position: '4', y_position: '4')
     expect(@king.valid_move?(6, 0)).to eq false
   end  
 end
