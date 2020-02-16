e868fc064e5a807d839370a44d7ae36d83a3053d
jaykibi <raquibbi@gmail.com>
2020-02-16 17:15:52 +0000

    added bishop valid_move? method and test, and fixed rook final test

--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Rook, type: :model do
     expect(@rook.valid_move?(4,5)).to eq true
   end
 
-   it 'should check if diagnonal moves are valid' do
+   it 'should check if diagonal moves are valid' do
     @rook = FactoryBot.create(:rook, x_position: '4', y_position: '4')
     expect(@rook.valid_move?(5,5)).to eq false
   end
@@ -21,10 +21,11 @@ RSpec.describe Rook, type: :model do
     expect(@rook.valid_move?(0,10)).to eq false
   end
 
-  # it 'should not be able to move passed another piece isObstructed' do
-  # 	game = FactoryBot.create(:game)
-  # 	@rook = FactoryBot.create(:rook, x_position: '4', y_position: '4', game_id: game.id)
-  # 	@rook2 = FactoryBot.create(:rook, x_position: '4', y_position: '3', game_id: game.id)
-  # 	expect(@rook.valid_move?(4,2)).to eq false
-  # end
+  it 'should not be able to move passed another piece isObstructed' do
+    Game.skip_callback(:create, :after, :populate_game!, raise: false)
+    game = FactoryBot.create(:game)
+  	@rook = FactoryBot.create(:rook, x_position: '4', y_position: '4', game_id: game.id)
+  	@rook2 = FactoryBot.create(:rook, x_position: '4', y_position: '3', game_id: game.id)
+  	expect(@rook.valid_move?(4,2)).to eq false
+  end
 end  
