f34d058b4894dc4a3a1e57d87343a4fb83ff8de4
jaykibi <raquibbi@gmail.com>
2020-02-17 04:45:13 +0000

    added spec for pawn and pawn movement logic

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,5 +1,50 @@
 class Pawn < Piece
-  def isObstructed? ()
-    
+  def valid_move?(x,y)
+    delta_x = x - self.x_position 
+    delta_y = y - self.y_position 
+    return false if reverse?(x,y)
+    return false if isObstructed?(x,y)
+    return false if !is_on_board?(x,y)
+    return true  if diagonal_capture?(x,y)
+    return false if delta_x != 0 
+    return true  if move_two?(x,y)
+    return true  if delta_y.abs == 1 
+    return false #if all else fails itll return false 
+  end
+
+  def reverse?(x,y)
+    delta_x = x - self.x_position
+    delta_y = y - self.y_position
+    return delta_y < 0 if self.color == 1
+    return delta_y > 0 if self.color == 2
+  end
+
+  def diagonal_capture?(x,y)
+    delta_x = x - self.x_position 
+    delta_y = y - self.y_position 
+    return false if delta_x.abs != 1
+    if self.color == 1 && delta_y == 1 || self.color == 2 && delta_y == -1 
+      enemies = game.pieces.find do |piece|
+        piece.x_position == x && piece.y_position == y 
+      end
+      return enemies.present? 
+    end 
+  end
+
+  def move_two?(x,y)
+    delta_x = (x - self.x_position).abs 
+    delta_y = (y - self.y_position).abs
+    return delta_y <= 2 if (self.color == 1 && y_position == 1) || (self.color == 2 && y_position == 6)
+    return false 
   end
 end
+
+
+# need to check valid move for pawn
+#   - in order to check for valid move
+#   - we need to check if target coordinate is:
+#           -  on board
+#           -  obstructed
+#           -  if it is an initial move, we can move 2 spaces
+#   - 
+#   - 
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,28 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe Pawn, type: :model do
-  # normal bishop movement test
-  it 'should test if PAWN can move 2 spaces on initial move' do 
-    Game.skip_callback(:create, :after, :populate_game!, raise: false)
-    game = FactoryBot.create(:game)
-    @bishop = FactoryBot.create(:bishop, x_position: '0', y_position: '0', game_id: game.id)
-    expect(@bishop.valid_move?(7,7)).to eq true
+  
+  it 'should test if WHITE PAWN can move 2 spaces on initial move' do 
+    @white_pawn = FactoryBot.create(:pawn, x_position: '3', y_position: '1', color: 1)
+    expect(@white_pawn.valid_move?(3,3)).to eq true
   end
 
-  it 'should test if PAWN cannot move 2 spaces after first move' do
+  it 'should test if BLACK PAWN can move 2 spaces on initial move' do 
+    @black_pawn = FactoryBot.create(:pawn, x_position: '3', y_position: '6', color: 2)
+    expect(@black_pawn.valid_move?(3,4)).to eq true
+  end
 
+  it 'should test if WHITE PAWN cannot move backwards(reverse)' do
+    @white_pawn = FactoryBot.create(:pawn, x_position: '3', y_position: '6', color: 1)
+    expect(@white_pawn.valid_move?(3,5)).to eq false
+  end
+
+  it 'should test if BLACK PAWN cannot move backwards(reverse)' do
+    @black_pawn = FactoryBot.create(:pawn, x_position: '3', y_position: '2', color: 2)
+    expect(@black_pawn.valid_move?(3,3)).to eq false
+  end
+
+ 
+  it 'should test if PAWN cannot move 2 spaces after first move' do
+    @pawn = FactoryBot.create(:pawn, x_position: '4', y_position: '4')
+    expect(@pawn.valid_move?(4,6)).to eq false
   end 
 
-  it 'should test if PAWN can move 1 space at a time' do
 
+  it 'should test if PAWN can move 1 space at a time' do
+    @pawn = FactoryBot.create(:pawn, x_position: '4', y_position: '4')
+    expect(@pawn.valid_move?(4,5)).to eq true
   end
 
-  it 'should test if PAWN isObstructed when attempting to move passed existing piece' do 
-
+  it 'should test if PAWN isObstructed when attempting to move passed an existing piece' do 
+    Game.skip_callback(:create, :after, :populate_game!, raise: false)
+    game = FactoryBot.create(:game)
+    @pawn = FactoryBot.create(:pawn, x_position: '4', y_position: '4', game_id: game.id)
+    @pawn2 = FactoryBot.create(:pawn, x_position: '4', y_position: '5', game_id: game.id)
+    expect(@pawn.valid_move?(4,5)).to eq false
   end
 
   it 'should test if PAWN can capture diagonally' do 
-
+    Game.skip_callback(:create, :after, :populate_game!, raise: false)
+    game = FactoryBot.create(:game)
+    @pawn = FactoryBot.create(:pawn, x_position: '4', y_position: '4', game_id: game.id)
+    @pawn2 = FactoryBot.create(:pawn, x_position: '5', y_position: '5', game_id: game.id)
+    expect(@pawn.valid_move?(5,5)).to eq true
   end 
-
 end
+
+# Game.skip_callback(:create, :after, :populate_game!, raise: false)
+# game = FactoryBot.create(:game)
+# @bishop = FactoryBot.create(:bishop, x_position: '0', y_position: '0', game_id: game.id)
+# expect(@bishop.valid_move?(7,7)).to eq true
