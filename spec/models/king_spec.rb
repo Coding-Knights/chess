f98b0abcecad3ab5e337b76ef299aee2eb865367
Joe <joeholley@me.com>
2020-02-18 22:37:48 +0000

    refactored king_spec.rb

--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -1,47 +1,44 @@
 require 'rails_helper'
 
-
-RSpec.describe Piece, type: :model do
-end
-
-RSpec.describe Game, type: :model do
-end
-
 RSpec.describe King, type: :model do
-  it 'should check if vertical moves are valid' do
-    @king = FactoryBot.create(:king, x_position: '4', y_position: '4')
-    expect(@king.valid_move?(4,5)).to eq true
+  describe 'valid_move? for the king' do
+    before(:each) do
+      @king = FactoryBot.create(:king, x_position: '4', y_position: '4')
+    end
+
+    it 'should check if vertical moves are valid' do
+      expect(@king.valid_move?(4,5)).to eq true
+    end
+
+    it 'should check if horizontal moves are valid' do
+      expect(@king.valid_move?(5,4)).to eq true
+    end
+
+    it 'should test if diagonal moves are valid' do
+      expect(@king.valid_move?(5,5)).to eq true
+    end
+
+    it 'should not allow horizontal movements greater than 1' do
+      expect(@king.valid_move?(6, 4)).to eq false
+    end
+
+    it 'should not allow vertical movements greater than 1' do
+      expect(@king.valid_move?(4, 6)).to eq false
+    end 
+
+    it 'should not allow diagonal movements greater than 1' do
+      expect(@king.valid_move?(6, 6)).to eq false
+    end
+
+    it 'should not allow move to original location' do
+      expect(@king.valid_move?(4, 4)).to eq false
+    end
+
+    it 'should check if king moving off board is invalid' do
+      @king = FactoryBot.create(:king, x_position: '0', y_position: '0')
+      expect(@king.valid_move?(0,10)).to eq false
+    end   
   end
-
-  it 'should check if horizontal moves are valid' do
-    @king = FactoryBot.create(:king, x_position: '4', y_position: '4')
-    expect(@king.valid_move?(5,4)).to eq true
-  end
-
-  it 'should test if diagonal moves are valid' do
-    @king = FactoryBot.create(:king, x_position: '4', y_position: '4')
-    expect(@king.valid_move?(5,5)).to eq true
-  end
-
-  it 'should not allow horizontal movements greater than 1' do
-    @king = FactoryBot.create(:king, x_position: '4', y_position: '4')
-    expect(@king.valid_move?(6, 4)).to eq false
-  end
-
-  it 'should not allow vertical movements greater than 1' do
-    @king = FactoryBot.create(:king, x_position: '4', y_position: '4')
-    expect(@king.valid_move?(4, 6)).to eq false
-  end 
-
-  it 'should not allow diagonal movements greater than 1' do
-    @king = FactoryBot.create(:king, x_position: '4', y_position: '4')
-    expect(@king.valid_move?(6, 6)).to eq false
-  end
-
-  it 'should not allow move to original location' do
-    @king = FactoryBot.create(:king, x_position: '4', y_position: '4')
-    expect(@king.valid_move?(4, 4)).to eq false
-  end   
 end
 
 
