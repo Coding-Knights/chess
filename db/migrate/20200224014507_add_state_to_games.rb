d9293a82b3ebc76b7aa857ba9aff2422b830023e
Joe <joeholley@me.com>
2020-02-24 18:31:34 +0000

    Updated message triggered when game state equals forfeited

--- db/migrate/20200224014507_add_state_to_games.rb
+++ db/migrate/20200224014507_add_state_to_games.rb
@@ -1,5 +1,5 @@
 class AddStateToGames < ActiveRecord::Migration[5.2]
   def change
-    add_column :games, :state, :string
+    add_column :games, :state, :string unless Game.column_names.include?('state')
   end
 end
