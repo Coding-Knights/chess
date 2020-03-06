384c49559d00d9daf1f64c0b220adff078745c7a
Al Trujillo <Trujillo0525@gmail.com>
2020-03-06 14:32:09 +0000

    added games won

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id]) 
-    @games_in_progress = Game.where(white_player_id: @user.id).or(Game.where(black_player_id: @user.id))   
+    @games_in_progress = Game.where(white_player_id: @user.id).or(Game.where(black_player_id: @user.id))
+    @games_won = Game.where(winner_id: @user.id)   
   end
 
-end
+end 
