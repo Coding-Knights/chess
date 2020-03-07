30bdeeb6fbe748911d04a00dc8c6626d654fab75
Al Trujillo <Trujillo0525@gmail.com>
2020-03-07 19:39:31 +0000

    added draw score

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @games_in_progress = Game.where(white_player_id: @user.id).or(Game.where(black_player_id: @user.id))
     @games_won = Game.where(winner_id: @user.id) 
     @games_lost = Game.where(loser_id: @user.id)  
-    #@games_draw = Game.where(state: @user.id)     For the draw games.  
+    @games_draw = Game.where(state: @user.id)       
   end
 
 end 
